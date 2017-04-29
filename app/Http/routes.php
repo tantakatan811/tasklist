ab1fd0c014b63f5e76e4b6c0818f0e2db948e20b
takashinakaya <tantakatan811@gmail.com>
2017-04-29 10:21:13 +0000

    CRUD tasks

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,11 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/','TasksController@index');
+Route::resource('tasks','TasksController');
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>タスク管理アプリ</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>

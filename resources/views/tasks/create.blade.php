c256a49dbaa89be11b045a20de4986c40e64ae9f
takashinakaya <tantakatan811@gmail.com>
2017-05-07 03:30:41 +0000

    lesson10 bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,17 +4,21 @@
     
     <h1>新規タスク作成ページ</h1>
     
-
-    {!! Form::model($task,['route' => 'tasks.store']) !!}
-        {!! Form::label('content','タスク：') !!}
-        {!! Form::text('content') !!}
+<div class="row">
+    <div class="col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+        {!! Form::model($task,['route' => 'tasks.store']) !!}
         
-        {!! Form::label('status','ステータス：') !!}
-        {!! Form::text('status') !!}
+        <div class="form-group">
+            {!! Form::label('content','タスク：') !!}
+            {!! Form::text('content',null,['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('status','ステータス：') !!}
+            {!! Form::text('status',null,['class' => 'form-control']) !!}
+        </div>
+            {!! Form::submit('投稿',['class' => 'btn btn-primary']) !!}
         
-        {!! Form::submit('投稿') !!}
-    
-    
-    {!! Form::close() !!}
-
+        {!! Form::close() !!}
+    </div>
+</div>
 @endsection 
